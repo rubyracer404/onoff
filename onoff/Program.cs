1b4455f8f24d1f192996ba620b42d49505893922
opilane <opilane@HP-CZC0359NNZ>
2024-02-27 14:15:01 +0200

    Add project files.

---
+++ onoff/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace onoff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] numbers = { 2, 4, 6, 8, 10 };
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                
+                Console.WriteLine(numbers[i]);
+                
+            }
+            Console.WriteLine("---------------");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i] + 10);
+            }
+            
+          
+        }
+    }
+}
